60ee24c48b3752bef0e7e0df80d2537059c4b40b
uzair <dotuxair@gmail.com>
2025-08-05 08:37:22 +0500

    Added User class to manage user data
    Introduced a new `User` class in the `ShortUrl.Server.Data.Entities` namespace. This class extends `BaseEntity` and includes properties for `Id`, `Email`, `FirstName`, `LastName`, `AvatarUrl`, `IsActive`, and `LastLoginAt`. The `Email`, `FirstName`, and `LastName` properties are required for instantiation.

---
+++ ShortUrl.Server/Data/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ShortUrl.Server.Data.Entities
+{
+    public class User : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public required string Email { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? AvatarUrl { get; set; }
+        public bool IsActive { get; set; }
+        public DateTimeOffset LastLoginAt { get; set; }
+    }
+}
