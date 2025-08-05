8a04fe7a31fa3f27626a67988797c319103227b2
uzair <dotuxair@gmail.com>
2025-08-05 08:35:37 +0500

    removed others methods.

--- ShortUrl.Server/Controllers/AuthController.cs
+++ ShortUrl.Server/Controllers/AuthController.cs
@@ -10,28 +10,13 @@ namespace ShortUrl.Server.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return ["value1", "value2"];
         }
 
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
-        }
-
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
