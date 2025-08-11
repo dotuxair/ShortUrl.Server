8e2bbc67ad8b06b681d6b16f9fe135e3b2250b11
uzair <dotuxair@gmail.com>
2025-08-11 23:10:23 +0500

    moved to Entities folder

--- ShortUrl.Server/Data/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShortUrl.Server.Data
-{
-    public class BaseEntity
-    {
-        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
-        public DateTimeOffset UpdatedAt { get; set; }
-    }
-}
