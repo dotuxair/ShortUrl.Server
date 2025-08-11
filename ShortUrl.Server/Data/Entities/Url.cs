a845b537c35771de812e6eadfc7399d8d1121be1
uzair <dotuxair@gmail.com>
2025-08-11 23:09:29 +0500

    table to store large and short urls

---
+++ ShortUrl.Server/Data/Entities/Url.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace ShortUrl.Server.Data.Entities
+{
+    public class Url
+    {
+
+        public BigInteger Id { get; set; }
+        public string OriginalUrl { get; set; } = string.Empty;
+        public string ShortUrl { get; set; } = string.Empty;
+
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
