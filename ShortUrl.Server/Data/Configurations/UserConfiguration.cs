43971c556d5ceb5b83d7909be9d6bfb3708fd50a
uzair <dotuxair@gmail.com>
2025-08-11 21:54:23 +0500

    removed required attribute from User class and added configuration file for user specific configurations.

---
+++ ShortUrl.Server/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShortUrl.Server.Data.Entities;
+
+namespace ShortUrl.Server.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("users");
+
+            builder.Property(u => u.Email)
+                   .IsRequired()
+                   .HasMaxLength(256);
+
+            builder.Property(u => u.FirstName)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(u => u.LastName)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+        }
+    }
+}
--- ShortUrl.Server/Data/Entities/User.cs
+++ ShortUrl.Server/Data/Entities/User.cs
@@ -3,11 +3,11 @@
     public class User : BaseEntity
     {
         public Guid Id { get; set; }
-        public required string Email { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public string? AvatarUrl { get; set; }
-        public bool IsActive { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string AvatarUrl { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
         public DateTimeOffset LastLoginAt { get; set; }
     }
 }
