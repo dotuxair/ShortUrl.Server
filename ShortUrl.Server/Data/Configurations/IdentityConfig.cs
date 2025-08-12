32e78b2a1429deb83d69124982a76e7dc5432fed
uzair <dotuxair@gmail.com>
2025-08-12 22:50:20 +0500

    Refactor: Customize Identity table mapping - Renamed tables: ApplicationUser → Users, UserLogins → UserLogins, UserTokens → UserTokens - Ignored unused role and claim tables (no role-based auth in use) - Centralized configuration in IdentityConfig extension for maintainability

---
+++ ShortUrl.Server/Data/Configurations/IdentityConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShortUrl.Server.Data.Entities;
+
+namespace ShortUrl.Server.Data.Configurations
+{
+    public static class IdentityConfig
+    {
+        public static void ConfigureIdentityModel(this ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("Users");
+            builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
+            builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
+
+            builder.Ignore<IdentityRole<Guid>>();
+            builder.Ignore<IdentityUserRole<Guid>>();
+            builder.Ignore<IdentityRoleClaim<Guid>>();
+            builder.Ignore<IdentityUserClaim<Guid>>();
+        }
+    }
+}
