309469a6a638d38ccc322ffbfdbb500789ad48dd
uzair <dotuxair@gmail.com>
2025-08-12 22:45:11 +0500

    registered AppDbContext and IdentityDbContext for migrations

--- ShortUrl.Server/Program.cs
+++ ShortUrl.Server/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ShortUrl.Server.Data;
+
 namespace ShortUrl.Server
 {
     public static class Program
@@ -6,6 +10,15 @@ namespace ShortUrl.Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ShortUrlDb"));
+            });
+
+            builder.Services.AddIdentityCore<IdentityUser<Guid>>(options =>
+            {
+                options.User.RequireUniqueEmail = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddSignInManager().AddDefaultTokenProviders();
 
             builder.Services.AddControllers();
             var app = builder.Build();
