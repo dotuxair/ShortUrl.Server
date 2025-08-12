41ce3e20ec50c23c6666eb7c2187ed63ab037c31
uzair <dotuxair@gmail.com>
2025-08-12 22:47:13 +0500

    DbContext class to configure tables

---
+++ ShortUrl.Server/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShortUrl.Server.Data.Configurations;
+using ShortUrl.Server.Data.Entities;
+
+namespace ShortUrl.Server.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<
+        ApplicationUser,
+        IdentityRole<Guid>,
+        Guid,
+        IdentityUserClaim<Guid>,
+        IdentityUserRole<Guid>,
+        IdentityUserLogin<Guid>,
+        IdentityRoleClaim<Guid>,
+        IdentityUserToken<Guid>>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ConfigureIdentityModel();
+        }
+    }
+}
